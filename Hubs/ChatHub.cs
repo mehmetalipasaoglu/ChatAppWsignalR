013b519ba85ce33a2d67220d08515a2d368790c4
Mehmet Ali Paşaoğulları <121023861+mehmetalipasaoglu@users.noreply.github.com>
2024-07-08 18:06:39 +0300

    Update ChatHub.cs

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,10 +1,22 @@
 using Microsoft.AspNetCore.SignalR;
 
-namespace ChatAppWsignalR.Hubs;
-
-public class ChatHub : Hub
+namespace ChatAppWsignalR.Hubs
 {
-    public async Task SendMessage(string user, string message){
-        await Clients.All.SendAsync("ReceiveMessage",user,message);
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message, string roomName)
+        {
+            await Clients.Group(roomName).SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task JoinGroup(string roomName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        }
+
+        public async Task LeaveGroup(string roomName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+        }
     }
 }
